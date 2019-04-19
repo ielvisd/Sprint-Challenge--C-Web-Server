506609424158a0fcdfd282f39822c747d7d254af
Elvis Ibarra <ielvisd@gmail.com>
2019-04-19 08:28:04 -0700

    Started to work on parser

--- src/client.c
+++ src/client.c
@@ -49,6 +49,19 @@ urlinfo_t *parse_url(char *url)
   // IMPLEMENT ME! //
   ///////////////////
 
+    //   We can parse the input URL by doing the following:
+
+    // 1. Use strchr to find the first backslash in the URL (this is assuming there is no http:// or https:// in the URL).
+    char *slash = strchr(path, '/');
+    printf("slash: %s\n", slash);
+    
+    // 2. Set the path pointer to 1 character after the spot returned by strchr.
+    // path = slash + 1;
+    // 3. Overwrite the backslash with a '\0' so that we are no longer considering anything after the backslash.
+    // 4. Use strchr to find the first colon in the URL.
+    // 5. Set the port pointer to 1 character after the spot returned by strchr.
+    // 6. Overwrite the colon with a '\0' so that we are just left with the hostname.
+
   return urlinfo;
 }
 
@@ -97,5 +110,15 @@ int main(int argc, char *argv[])
   // IMPLEMENT ME! //
   ///////////////////
 
+  else {
+    // 1. Parse the input URL
+    // parse_url();
+    printf("HERE!");
+    printf(argv[1]);
+    // 2. Initialize a socket by calling the `get_socket` function from lib.c
+    // 3. Call `send_request` to construct the request and send it
+    // 4. Call `recv` in a loop until there is no more data to receive from the server. Print the received response to stdout.
+    // 5. Clean up any allocated memory and open file descriptors.
+}
   return 0;
 }
